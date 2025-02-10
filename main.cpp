2794bc9fb66c7d5da22c49631a9f5ca5adf31df2
XeTute Technologies <x3tut3@gmail.com>
2025-02-10 14:12:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,13 +16,12 @@ int main()
 		std::cout << "nthreads: ";
 		std::cin >> nthreads;
 
-		std::cout << "nelems ( / 1 000 000): ";
+		std::cout << "nelems: ";
 		std::cin >> nelems;
 
-		nelems *= 1000000;
 		con.resize(nelems);
 
-		omp_set_num_threads(nthreads);
+		omp_set_num_threads(int(nthreads));
 
 		for (EC::n i = 0; i < signed(con.size()); ++i)
 			con[i] = 15.25f;
@@ -32,7 +31,7 @@ int main()
 
 		tp[0] = { std::chrono::high_resolution_clock::now() };
 #pragma omp parallel for
-		for (std::intmax_t i = 0; i < con.size(); ++i)
+		for (std::intmax_t i = 0; i < signed(con.size()); ++i)
 			con[i] *= con[i];
 		tp[1] = std::chrono::high_resolution_clock::now();
 		td[0] = std::chrono::duration_cast<std::chrono::milliseconds>(tp[1] - tp[0]).count();
@@ -42,7 +41,7 @@ int main()
 			con[i] = 15.25f;
 
 		tp[0] = std::chrono::high_resolution_clock::now();
-		con.mult(con);
+		con *= con;
 		tp[1] = std::chrono::high_resolution_clock::now();
 		td[1] = std::chrono::duration_cast<std::chrono::milliseconds>(tp[1] - tp[0]).count();
 		std::cout << "EC took     " << td[1] << "ms.\n";
@@ -53,6 +52,8 @@ int main()
 			std::cout << "OpenMP was as fast as EC.\n";
 		else
 			std::cout << "EC was faster than OpenMP.\n";
+
+		std::cout << "Last Element: " << con[con.size() - 1] << '\n';
 		std::cout << "--- --- ---" << std::endl;
 	}
 
